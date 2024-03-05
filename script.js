eda4bf4a0b9772e14f16376f2539d157d5045319
Abir Chodha <abirchodha1@gmail.com>
2024-03-05 21:56:53 +0530

    Change background color on winning

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
 
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
