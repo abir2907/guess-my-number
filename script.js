a8ff8e0f92eb937e3c6f2ca1bb68442085aabc4d
Abir Chodha <abirchodha1@gmail.com>
2024-03-05 22:03:58 +0530

    Display number on winning

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     // When guess is too high
