968eb5e5c3462921e7c6dd7d900cd0ca039d9878
Abir Chodha <abirchodha1@gmail.com>
2024-03-05 21:34:38 +0530

    Add logic for when guess is correct

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
   }
 });
